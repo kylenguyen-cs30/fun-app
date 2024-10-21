dab0b0e00fced74c5c73096fe5cf589317ee1511
Kyle Nguyen <knguyen1193@outlook.com>
2024-10-21 11:00:41 -0700

    finishing demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import styles from './page.module.css';
+	let targetNumber;
+	let guessNumber = '';
+	let message = '';
+
+	// function to generate a random number
+	function generateTargetNumber() {
+		targetNumber = Math.floor(Math.random() * 101);
+		message = 'Guess a number between 0 to 100!';
+	}
+
+	// function to check
+	function checkGuess() {
+		if (parseInt(guessNumber) === targetNumber) {
+			message = 'Congratualtions! you guessed it right!';
+		} else if (parseInt(guessNumber) < targetNumber) {
+			message = 'Too Low! Try again';
+		} else {
+			message = 'Too High! Try again';
+		}
+	}
+
+	// Function to reset
+	function resetGame() {
+		generateTargetNumber();
+		guessNumber = '';
+		message = '';
+	}
+
+	// initialize the game
+	onMount(() => {
+		resetGame();
+	});
+</script>
+
+<div class={styles.bigDiv}>
+	<h1 class={styles.welcomeHeader}>Welcome to SvelteKit</h1>
+
+	<div>
+		<input type="number" bind:value={guessNumber} placeholder="Enter your guess" />
+		<button on:click={checkGuess}>Check</button>
+		<button on:click={resetGame}>Reset</button>
+	</div>
+
+	<p>{message}</p>
+</div>
